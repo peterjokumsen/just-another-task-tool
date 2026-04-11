7bf16b05bc1d91c13fda2c89f03aaeb467107ec0
Peter Jokumsen <pjokumsen@gmail.com>
2026-04-12 01:24:34 +0200

    change to jatt-api to force build

--- apps/jatt-api/Program.cs
+++ apps/jatt-api/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-// a change to reset cache
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
