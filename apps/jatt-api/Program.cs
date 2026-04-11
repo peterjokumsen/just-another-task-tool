7585311c181f06bf7db7dad2dd13f3ae3cd1e231
Peter Jokumsen <pjokumsen@gmail.com>
2026-04-11 21:28:30 +0200

    chore: minor change to reset cache for jatt-api

--- apps/jatt-api/Program.cs
+++ apps/jatt-api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+// a change to reset cache
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
