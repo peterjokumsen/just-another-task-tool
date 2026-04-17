7db2e30dfdb84294cddce05db1d38ae90464c8c8
Peter Jokumsen <pjokumsen@gmail.com>
2026-04-17 22:59:32 +0200

    feat: add landing pages and migrate mobile to use Expo

--- apps/jatt-web/src/app/app.routes.ts
+++ apps/jatt-web/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./landing/landing.component').then((m) => m.LandingComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./auth/signup.component').then((m) => m.SignupComponent),
+  },
+];
